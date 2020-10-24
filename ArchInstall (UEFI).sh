0557244a8216a20cddaca927f53c52cf87aac50a
nwon24 <60217282+nwon24@users.noreply.github.com>
2020-10-24 15:53:17 +1100

    Update ArchInstall (UEFI).sh

--- ArchInstall (UEFI).sh
+++ ArchInstall (UEFI).sh
@@ -66,11 +66,7 @@ EOF
   mkswap ${disk}2
   mkfs.ext4 ${disk}3
   mkfs.ext4 ${disk}4
-<<<<<<< HEAD
   mount $(disk)3 /mnt
-=======
-  mount ${disk}3 /mnt
->>>>>>> 2a85998e78bd8726bd903eb9dddcfe0664a8870a
   mkdir /mnt/efi && mkdir /mnt/home
   mount ${disk}1 /mnt/efi
   mount ${disk}4 /mnt/home
@@ -122,6 +118,7 @@ cat <<EOF > /mnt/chroot.sh
 EOF
 sed 's/  //g' /mnt/chroot/sh
 arch-chroot /mnt /chroot.sh
+rm -rf /mnt/chroot.sh
 umount -a
 swapoff ${disk}2
 reboot
