3ad0e14640ca02addb2aea567eb3488dfa7ccafc
nwon24 <60217282+nwon24@users.noreply.github.com>
2020-10-24 20:20:17 +1100

    Update ArchInstallBios.sh

--- ArchInstallBios.sh
+++ ArchInstallBios.sh
@@ -1,1 +1,145 @@
+echo "This is an arch install script for a system booted into BIOS mode."
+pacman -Sy
+lsblk
 
+echo 'Enter disk name (e.g. /dev/sda) '
+read disk
+disk_size = blockdev --getsize64 $disk
+root_size = (disk_size - 2,147,483,648) * 10 ** -9
+home_size = root_size - 24
+echo 'If installing Arch over an existing operating system, fdisk will ask if you if you want to continue. Type in y and then enter to continue.'
+echo 'Would you like a separate home partition? (Y/N) '
+read home
+if [ $home = N ]
+then
+  fdisk $disk <<EOF
+  o
+  n
+  p
+  1
+  
+  +${root_size}G
+  a
+  n
+  p
+  2
+  
+  
+  t
+  2
+  82
+  w
+EOF
+  mkswap ${disk}2
+  mkfs.ext4 ${disk}1
+  mount ${disk}1 /mnt
+  swapon ${disk}2
+  swap_part = ${disk}2
+else
+  fdisk $disk <<EOF
+  o
+  n
+  p
+  1
+  
+  +24G
+  a
+  n
+  p
+  2
+  
+  +${home_size}G
+  n
+  p
+  3
+  
+  
+  t
+  3
+  82
+  w
+EOF
+  mkswap ${disk}3
+  mkfs.ext4 ${disk}2
+  mkfs.ext4 ${disk}1
+  mount $(disk)1 /mnt
+  mkdir /mnt/home
+  mount ${disk}2 /mnt/home
+  swapon ${disk}3
+  swap_part = ${disk}3
+fi
+echo 'Would you like to edit the pacman mirror list before installing the base system? (Y/N) '
+read mirrorlist
+if [ $mirrorlist = Y ] 
+then 
+  nano /etc/pacman.d/mirrorlist
+fi 
+echo 'Which kernel would you like to install? [l]inux, linux-lt[s], linux-[z]en '
+read kernel
+if [ $kernel = lz ]
+then 
+  pacstrap /mnt base linux-zen linux-firmware base-devel vim nano
+elif [ $kernel = s ] 
+then 
+  pacstrap /mnt base linux-lts linux-firmware base-devel vim nano
+else 
+  pacstrap /mnt base linux linux-firmware base-devel vim nano
+fi
+genfstab -U /mnt >> /mnt/etc/fstab
+cat <<EOF > /mnt/chroot.sh
+  ls /usr/share/zoneinfo/
+  echo 'Enter timezone reigon (look at above if unsure) '
+  read reigon
+  ls /usr/share/zoneinfo/$region/
+  echo 'Enter timezone city (look at avobe if unsure) '
+  read city
+  ln -sf /usr/share/zoneinfo/$country/$city /etc/localtime
+  timedatectl set-ntp true
+  hwclock --systohc
+  echo 'Enter hostname '
+  read hostname
+  echo $hostname > /etc/hostname
+  cat <<EOF > /etc/hosts
+  127.0.0.1	localhost
+  ::1		localhost
+  127.0.1.1	$hostname.localdomain $hostname
+  EOF
+  sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+  echo 'Do you need other locales other than en_US.UTF-8? [Y/N] '
+  read locale_edit
+  if [ locale_edit == Y ] 
+  then 
+    echo 'Comment out needed locales from the locale.gen file.'
+    sleep 6
+    nano /etc/locale.gen
+  fi
+  locale-gen
+  echo 'What would you like to set the LANG variable in locale.conf? (Press enter for en_US.UTF-8) '
+  read locale
+  if [ $locale = '' ] 
+  then 
+    echo LANG=en_US.UTF-8 > /etc/locale.conf
+  else
+    echo LANG=$locale > /etc/locale.conf
+  fi
+  echo 'Set root password'
+  passwd root
+  echo '[I]ntel or [A]MD ucode? '
+  read ucode
+  if [ $ucode == I ]
+  then
+    pacman -S grub networkmanager intel-ucode 
+  else
+    pacman -S grub networkmanager amd-ucode
+  fi
+  grub-install --target=i386-pc $disk
+  grub-mkconfig -o /boot/grub/grub.cfg
+  systemctl enable NetworkManager 
+EOF
+chmod 777 /mnt/chroot.sh
+sed -i 's/  //g' /mnt/chroot.sh
+arch-chroot /mnt /chroot.sh
+rm -rf /mnt/chroot.sh
+umount -a
+swapoff $swap_part
+reboot
