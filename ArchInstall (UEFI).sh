22c3d6de619fcbe3e2f30f974cdee9959cf2b0fe
root <nwonb@schools.vic.edu.au>
2020-10-24 04:43:11 +0000

    Added chroot portion

--- ArchInstall (UEFI).sh
+++ ArchInstall (UEFI).sh
@@ -28,14 +28,14 @@ then
 
 
   w
-  EOF
-  mkfs.fat -F32 ($disk)1
-  mkswap ($disk)2
-  mkfs.ext4 ($disk)3
-  mount ($disk)3 /mnt
+EOF
+  mkfs.fat -F32 ${disk}1
+  mkswap ${disk}2
+  mkfs.ext4 ${disk}3
+  mount ${disk}3 /mnt
   mkdir /mnt/efi
-  mount ($disk)1 /mnt/efi
-  swapon ($disk)2
+  mount ${disk}1 /mnt/efi
+  swapon ${disk}2
 else
   fdisk $disk <<EOF
   g
@@ -61,16 +61,16 @@ else
   
   
   w
-  EOF
-  mkfs.fat -F32 ($disk)1
-  mkswap ($disk)2
-  mkfs.ext4 ($disk)3
-  mkfs.ext4 ($disk)4
-  mount ($disk3) /mnt
+EOF
+  mkfs.fat -F32 ${disk}1
+  mkswap ${disk}2
+  mkfs.ext4 ${disk}3
+  mkfs.ext4 ${disk}4
+  mount $(disk)3 /mnt
   mkdir /mnt/efi && mkdir /mnt/home
-  mount ($disk)1 /mnt/efi
-  mount ($disk)4 /mnt/home
-  swapon ($disk)2
+  mount ${disk}1 /mnt/efi
+  mount ${disk}4 /mnt/home
+  swapon ${disk}2
 fi
 echo 'Which kernel would you like to install? [l]inux, linux-lt[s], linux-[z]en '
 read kernel
@@ -84,5 +84,40 @@ else
   pacstrap /mnt base linux-zen base-devel vim
 fi
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-  
+cat <<EOF > /mnt/chroot.sh
+  echo 'Enter timezone (e.g. Australia/Melbourne) '
+  read timezone
+  ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+  timedatectl set-ntp true
+  hwclock --systohc
+  echo 'Enter hostname '
+  read hostname
+  echo $hostname > /etc/hostname
+  cat <<EOF > /etc/hosts
+  127.0.0.1	localhost
+  ::1		localhost
+  127.0.1.1	$hostname.localdomain $hostname
+  EOF
+  sed -i 's/#en_AU.UTF-8 UTF-8/en_AU.UTF-8 UTF-8/g' /etc/locale.gen
+  sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+  locale-gen
+  echo LANG=$locale > /etc/locale.conf
+  echo 'Set root password'
+  passwd root
+  echo '[I]ntel or [A]MD ucode? '
+  read ucode
+  if [ $ucode == I ]
+  then
+    pacman -S grub efibootmgr networkmanager intel-ucode 
+  else
+    pacman -S grub efibootmgr networkmanager amd-ucode
+  fi
+  grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+  grub-mkconfig -o /boot/grub/grub.cfg
+  systemctl enable NetworkManager 
+EOF
+sed 's/  //g' /mnt/chroot/sh
+arch-chroot /mnt /chroot.sh
+umount -a
+swapoff ${disk}2
+reboot
